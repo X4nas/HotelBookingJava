3422a5754648693afa412d3e3129c003ea9c0046
X4nas <md.anas1028@gmail.com>
2025-05-23 11:17:01 +0530

    added Reservation

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,2 +1,16 @@
 public class Reservation {
+    String guestName, phone, roomType, checkIn, checkOut;
+
+    public Reservation(String guestName, String phone, String roomType, String checkIn, String checkOut) {
+        this.guestName = guestName;
+        this.phone = phone;
+        this.roomType = roomType;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public String toString() {
+        return "Guest: " + guestName + ", Phone: " + phone + ", Room: " + roomType +
+                ", Check-In: " + checkIn + ", Check-Out: " + checkOut;
+    }
 }
